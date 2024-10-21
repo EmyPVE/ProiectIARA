628bca14f4a4d15cb05b7988db59b4e368cbf4f8
EmyPVE <emyyy02@gmail.com>
2024-10-21 18:38:58 +0300

    Initial Commit

---
+++ ProiectIARATests/ProiectIARATests.swift
@@ -0,0 +1,17 @@
+//
+//  ProiectIARATests.swift
+//  ProiectIARATests
+//
+//  Created by Emanuel Prelipcean on 21.10.2024.
+//
+
+import Testing
+@testable import ProiectIARA
+
+struct ProiectIARATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
